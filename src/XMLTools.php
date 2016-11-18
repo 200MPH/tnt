202c80bd3edc261a760f0b4c969fe5565227c188
Wojciech Brozyna <wojciechbrozyna@importservices.co.uk>
2016-11-18 12:31:24 +0000

    Created function for document merge. Created test for above function.

--- src/XMLTools.php
+++ src/XMLTools.php
@@ -8,6 +8,8 @@
 
 namespace thm\tnt_ec;
 
+use DOMDocument;
+
 class XMLTools {
     
     /**
@@ -19,18 +21,51 @@ class XMLTools {
     static public function mergeXml(array $documents)
     {
         
-        foreach($documents as $document) {
+        $merged = null;
+        
+        // always merge firts elements of array with the others
+        // therefore we have to subtract one position form array
+        // otherwise we will get undefined offest error 
+        $counter = count($documents) - 1;
+        
+        for($i=0; $i < $counter; $i++) {
             
             // skip empty documents
-            if(empty($document) === true) {
+            if(empty($documents[$i]) === true) { continue; }
+            
+            if($i === 0) {
                 
-                continue;
+                // add first document to merge array.
+                $merged = $documents[$i];
                 
             }
             
+            // load parent document
+            $parent = new DOMDocument();
+            $parent->loadXML($merged);
+                                   
+            // load child document, next in the array
+            $child = new DOMDocument();
+            $child->loadXml($documents[$i+1]);
             
+            foreach ($child->documentElement->childNodes as $node) {
+                
+                $importedNode = $parent->importNode($node, true);
+                
+                $parent->documentElement->appendChild($importedNode);
+                
+                $merged = $parent->saveXML();
+                
+            }
             
         }
+        
+        // dont know why, but setting up parent property like below
+        // does not remove new line character from document
+        // so I used str_replace instead
+        // $parent->preserveWhiteSpace = false;
+        // $parent->formatOutput = false; 
+        return str_replace("\n", '', $merged);
         
     }
     
--- test/XMLToolsTest.php
+++ test/XMLToolsTest.php
@@ -18,9 +18,11 @@ class XMLToolsTest  extends \PHPUnit_Framework_TestCase {
     public function testMerge()
     {
         
-        $xml[] = '';
-        $xml[] = '';
-        $result = '';
+        $xml[] = '<?xml version="1.0"?><track><consignment><item>1</item></consignment><consignment><item>2</item></consignment></track>';
+        $xml[] = '<?xml version="1.0"?><track><consignment><item>3</item></consignment><consignment><item>4</item></consignment></track>';
+        $xml[] = null; // this must be skipped by function
+        $xml[] = '<?xml version="1.0"?><track><consignment><item>5</item></consignment><consignment><item>6</item></consignment></track>';
+        $result = '<?xml version="1.0"?><track><consignment><item>1</item></consignment><consignment><item>2</item></consignment><consignment><item>3</item></consignment><consignment><item>4</item></consignment><consignment><item>5</item></consignment><consignment><item>6</item></consignment></track>';
         
         $this->assertEquals($result, XMLTools::mergeXml($xml));
         
