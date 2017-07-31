2cf5b5e4e177511585f2f88ffda05db5638e62a1
Wojtek <wojciech.brozyna@gmail.com>
2017-07-31 19:31:40 +0200

    Cast consignment and alternative consignment to string.

--- src/Service/TrackingService/libs/Consignment.php
+++ src/Service/TrackingService/libs/Consignment.php
@@ -94,7 +94,7 @@ class Consignment {
         
         if(isset($this->xml->ConsignmentNumber) === true) {
             
-            return $this->xml->ConsignmentNumber;
+            return $this->xml->ConsignmentNumber->__toString();;
             
         }
         
@@ -112,7 +112,7 @@ class Consignment {
         
         if(isset($this->xml->AlternativeConsignmentNumber) === true) {
             
-            return $this->xml->AlternativeConsignmentNumber;
+            return $this->xml->AlternativeConsignmentNumber->__toString();;
             
         }
         
