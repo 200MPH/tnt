957c2a8e4a0e8887dc625ce4ac32bab698353dfb
Wojciech Brozyna <wojciech.brozyna@gmail.com>
2018-01-16 15:09:44 +0100

    Added autoloader to test class.

--- test/ShippingServiceRawTests.php
+++ test/ShippingServiceRawTests.php
@@ -2,6 +2,7 @@
 
 /* This is a place for testing Shipping functionality */
 
+require_once __DIR__ . '/../vendor/autoload.php';
 use thm\tnt_ec\service\ShippingService\ShippingService;
 
 $shipping = new ShippingService('123', '123');
